0d6cac9490eef1ad944024a985b94412a9aead24
Utako Ueda <utakoueda@gmail.com>
2013-02-11 16:11:42 -0800

    update

--- variance.rb
+++ variance.rb
@@ -15,28 +15,34 @@ f_numbers
 
 class Array
   
+  # Find sum
   def sum
     inject(nil) { |sum, x| sum ? sum + x : x }
   end
   
+  # Find mean
   def mean
     sum.to_f / size.to_f  
   end
   
+  # Find population variance
   def pvariance
     value = inject(0) {|value, x| value ? value + (x - mean)**2 : x}
     value.to_f / size.to_f
   end
   
+  # Find sample variance
   def svariance
     value = inject(0) {|value, x| value ? value + (x-mean)**2 : x}
     value.to_f / (size.to_f-1)
   end
   
+  # Find population standard deviation
   def psdev
     Math.sqrt(pvariance)
   end
   
+  # Find sample standard deviation
   def ssdev
     Math.sqrt(svariance)
   end
